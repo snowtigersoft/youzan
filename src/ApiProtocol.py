4b9c9c16f4fe9cf480e1a9da468cbebe6ae23089
kylinlin <kylinlingh@foxmail.com>
2016-01-13 10:51:08 +0800

    添加文件头"

--- src/ApiProtocol.py
+++ src/ApiProtocol.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 - kylinlingh@foxmail.com
 import hashlib
 
 
--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 - kylinlingh@foxmail.com
+
 import xml.etree.ElementTree as etree
 from threading import Thread
 import re
